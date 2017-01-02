c0c6e8251d280f5a35c6ba5fa121320f901e195e
chingizy@gmail.com <chingizy@gmail.com>
2017-01-02 12:25:28 +0300

    sliffki table

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Слиффки - заказы</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/styles.css">
+  	<script src="js/jquery.min.js"></script>
+	<script src="js/main.js"></script>
+</head>
+<body>
+    
+
+</body>
+</html>
